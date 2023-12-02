0da52df0eda6130c5b08771255d6fdfe1de4c2e8
MahaNivetha <150665663+MahaNivetha@users.noreply.github.com>
2023-12-02 11:39:07 +0530

    chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -1,34 +1,30 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
-import sys
-import re
 from RSA import decrypt
+
 PORT_NUMBER = 5000
 SIZE = 1024
 
+hostName = gethostbyname('localhost')
 
-hostName = gethostbyname( 'localhost' )
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.bind((hostName, PORT_NUMBER))
 
+print("Test server listening on port {0}\n".format(PORT_NUMBER))
+client_public_key = ''
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-mySocket.bind( (hostName, PORT_NUMBER) )
-
-print ("Test server listening on port {0}\n".format(PORT_NUMBER))
-client_public_key=''
 while True:
-        (data,addr) = mySocket.recvfrom(SIZE)
-        data=data.decode()
-        if data.find('public_key')!=-1: #client has sent their public key\
-            
-            public = (int(re.findall(r'\d+', data)[0]), int(re.findall(r'\d+', data)[1]))
-            print('public key is : %d, %d'%(public[0],public[1]))
-            
-        else:
-            cipher=int(data)
-            print (str(cipher)+':')
-            
-            data_decoded= decrypt(public, cipher)
-            print (data_decoded)
-            #python2: print data ,
-sys.ext()
+    data, addr = mySocket.recvfrom(SIZE)
+    data = data.decode()
 
+    if 'public_key' in data:
+        # Client has sent their public key
+        public = tuple(map(int, re.findall(r'\d+', data)))
+        print('Public key is: %d, %d' % (public[0], public[1]))
 
+    else:
+        cipher = int(data)
+        print(str(cipher) + ':')
+
+        data_decoded = decrypt(public, cipher)
+        print(data_decoded)
+        # python2: print data ,
