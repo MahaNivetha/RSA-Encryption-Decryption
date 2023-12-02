d3131230c11f787e9e7e5e5b104194a7682aa42b
MahaNivetha <150665663+MahaNivetha@users.noreply.github.com>
2023-12-02 11:38:46 +0530

    chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -1,28 +1,28 @@
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
-from RSA import generate_keypair,encrypt,decrypt
+from RSA import generate_keypair, encrypt, decrypt
 
-# SERVER_IP    = gethostbyname( 'DE1_SoC' )
-SERVER_IP    = gethostbyname( 'localhost' )
+SERVER_IP = gethostbyname('localhost')
 PORT_NUMBER = 5000
 SIZE = 1024
-print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-message='hello'
+print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
 
-p=1297273
-q=1297651
+mySocket = socket(AF_INET, SOCK_DGRAM)
+message = 'hello'
 
-public, private = generate_keypair(p,q)
-message=('public_key: %d %d' % (public[0], public[1]))
-mySocket.sendto(message.encode(),(SERVER_IP,PORT_NUMBER))
+p = 1297273
+q = 1297651
+
+public, private = generate_keypair(p, q)
+message = 'public_key: %d %d' % (public[0], public[1])
+mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
+
 while True:
-        message=input()
-        message.join('\n')
-        
-        message_encoded=[]
-        for i in message:
-                message_encoded.append(str(encrypt(private,i)))
-        [mySocket.sendto(code.encode(),(SERVER_IP,PORT_NUMBER)) for code in message_encoded]
+    message = input()
+    message.join('\n')
+
+    message_encoded = [str(encrypt(private, i)) for i in message]
+    [mySocket.sendto(code.encode(), (SERVER_IP, PORT_NUMBER)) for code in message_encoded]
+
 sys.exit()
